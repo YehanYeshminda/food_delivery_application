f390ccb75cfe4e7d2288173860228677f0300748
YehanYeshminda <81465670+YehanYeshminda@users.noreply.github.com>
2022-08-06 00:26:53 +0530

    added responsive nav bar and grids

--- src/components/MainContainer.jsx
+++ src/components/MainContainer.jsx
@@ -1,7 +1,12 @@
 import React from 'react';
 
 const MainContainer = () => {
-	return <div>MainContainer</div>;
+	return (
+		<div className="grid grid-cols-1 md:grid-cols-2 gap-2">
+			<div className="py-2 flex-1"></div>
+			<div className="py-2 flex-1"></div>
+		</div>
+	);
 };
 
 export default MainContainer;

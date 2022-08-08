ed738fc7ba5e5b813f51ced424e8b2be4c33e177
YehanYeshminda <81465670+YehanYeshminda@users.noreply.github.com>
2022-08-08 07:04:07 +0530

    added the button for the addition of the catergories

--- src/components/LoadCreateContainer.jsx
+++ src/components/LoadCreateContainer.jsx
@@ -25,6 +25,8 @@ const LoadCreateContainer = () => {
 
 	const deleteImage = () => {};
 
+	const saveDetails = () => {};
+
 	return (
 		<div className="w-full min-h-screen flex items-center justify-center">
 			<div className="w-[90%] md:w-[75%] border border-gray-300 p-4 flex flex-col items-center justify-center gap-4">
@@ -150,6 +152,16 @@ const LoadCreateContainer = () => {
 							className="w-full h-full text-lg bg-transparent outline-none border-none placeholder:text-gray-400 text-textColor"
 						/>
 					</div>
+				</div>
+
+				<div className="flex items-center w-full">
+					<button
+						type="button"
+						className="ml-0 md:ml-auto w-full md:w-auto border-none outline-none bg-emerald-500 px-12 py-2 rounded-lg text-lg text-white font-semibold"
+						onClick={saveDetails}
+					>
+						Save
+					</button>
 				</div>
 			</div>
 		</div>

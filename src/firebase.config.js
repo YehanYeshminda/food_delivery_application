9d9220607ddce5869ae361786f13ec1a4ea2e208
YehanYeshminda <81465670+YehanYeshminda@users.noreply.github.com>
2022-08-18 11:02:32 +0530

    added some secure methods

--- src/firebase.config.js
+++ src/firebase.config.js
@@ -4,14 +4,13 @@ import { getStorage } from 'firebase/storage';
 
 // Your web app's Firebase configuration
 const firebaseConfig = {
-	apiKey: 'AIzaSyBvfeIgclaehhVJvUKRvIsxQwDkRz5K_II',
-	authDomain: 'food-delivery-applicatio-16952.firebaseapp.com',
-	databaseURL:
-		'https://food-delivery-applicatio-16952-default-rtdb.firebaseio.com',
-	projectId: 'food-delivery-applicatio-16952',
-	storageBucket: 'food-delivery-applicatio-16952.appspot.com',
-	messagingSenderId: '366919523429',
-	appId: '1:366919523429:web:ff785d44f2038ef2deba8c',
+	apiKey: process.env.REACT_APP_FIREBASE_API_KEY,
+	authDomain: process.env.REACT_APP_FIREBASE_AUTH_DOMAIN,
+	databaseURL: process.env.REACT_APP_FIREBASE_DATABASE_URL,
+	projectId: process.env.REACT_APP_FIREBASE_PROJECT_ID,
+	storageBucket: process.env.REACT_APP_FIREBASE_STORAGE_BUCKET,
+	messagingSenderId: process.env.REACT_APP_FIREBASE_MESSAGE_SENDER_ID,
+	appId: process.env.REACT_APP_FIREBASE_APP_ID,
 };
 
 const app = getApps.length > 0 ? getApp() : initializeApp(firebaseConfig);
